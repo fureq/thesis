5229678dbf86438c7ccbac871a37f4523c961297
fureq <furmanszymon@gmail.com>
2018-04-10 14:45:50 +0200

    Fix ROI

--- src/Scripts/ROI.py
+++ src/Scripts/ROI.py
@@ -3,8 +3,13 @@ import src.images.ImagesHandler as ImagesHandler
 import src.images.ImagesTransformator as ImagesTransformator
 
 COILD_ID = 675593
-DEFECT_NO = 10
+DEFECT_NO = 14
 CAMERA_NO = 12
+
+X0 = 0
+X1 = 1
+Y0 = 2
+Y1 = 3
 
 dbHandler = DatabaseHandler.DatabaseHandler()
 imgHandler = ImagesHandler.ImagesHandler()
@@ -14,5 +19,6 @@ img = imgHandler.getImageBasedOnDatabaseRecord(COILD_ID, CAMERA_NO, DEFECT_NO)
 
 roi = dbHandler.getROIOfRecord(COILD_ID, CAMERA_NO, DEFECT_NO)
 
-transfomrmedImg = imgTransformator.createImageBasedOnROI(img, roi[0], roi[1], roi[2], roi[3])
+transfomrmedImg = imgTransformator.createImageBasedOnROI(img, roi[X0], roi[X1], roi[Y0], roi[Y1])
+print '(' + str(roi[X0]) + ',' + str(roi[Y0]) + ') (' + str(roi[X1]) + ',' + str(roi[Y1]) + ')'
 imgHandler.showImage(transfomrmedImg)
